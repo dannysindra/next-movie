52226f7568a77ac040ba5559b218378af340b99c
dannysindra <als478@cornell.edu>
2019-11-03 13:06:21 -0800

    prettier

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './app.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./app.css";
 
 export const App = () => {
   return (
