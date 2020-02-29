ccdc8d957654785986d60cf63f2b8ddc06b03c32
dannysindra <als478@cornell.edu>
2020-02-29 01:33:19 -0800

    loading animation

--- src/styles/hooks.js
+++ src/styles/hooks.js
@@ -8,3 +8,26 @@ export const usePrimaryColor = (styles = {}) => {
         ...styles
     });
 };
+
+// https://github.com/mui-org/material-ui/blob/master/packages/material-ui-lab/src/Skeleton/Skeleton.js
+export const usePulseAnimation = () => {
+    const [useCss] = useStyletron();
+
+    return useCss({
+        animationDuration: '1.5s',
+        animationDelay: '0.5s',
+        animationTimingFunction: 'ease-in-out',
+        animationIterationCount: 'infinite',
+        animationName: {
+            '0%': {
+                opacity: 1
+            },
+            '50%': {
+                opacity: 0.4
+            },
+            '100%': {
+                opacity: 1
+            }
+        }
+    });
+};
